47988a1985071097c7e6b5eb0fab7c1abf65d513
aljomatrix <aljomatrix@hotmail.com>
2024-10-09 15:11:11 +0200

    Baseline

---
+++ Assignment2/Assignment2/View/GameWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Assignment2.View
+{
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
